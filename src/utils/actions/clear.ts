35b5a9d1b5ac97d97360a9f4bc071c57d5501d87
westy12dan <westy12dan@gmail.com>
2025-02-04 22:45:58 +0800

    clear clears console too

--- src/utils/actions/clear.ts
+++ src/utils/actions/clear.ts
@@ -1,11 +1,12 @@
 import type { History } from "../../types";
 import { HISTORY_CACHE_FILE } from "../history";
 import fs from "fs";
-import { Color, printLine } from "../io";
+import { clearConsole, Color, printLine } from "../io";
 
 export function clear(history: History) {
   fs.rmSync(HISTORY_CACHE_FILE);
   history.length = 0;
+  clearConsole();
   printLine();
   printLine("Cleared History", Color.Gray);
 }
